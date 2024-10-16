ae75d14834ed52351ffe76dd8cdade179770914b
muhammad haseen <hasinmmpm786@gmail.com>
2024-10-16 10:53:41 +0530

    updated skills

--- src/Components/Skils.jsx
+++ src/Components/Skils.jsx
@@ -17,29 +17,46 @@ const techSkill = [
   "Photoshop",
   "Mongoose",
   "JWT",
+  "Responsive Web Design",
+  "RESTful API",
+  "git",
+  "React Hooks",
 ];
 
 const softSkill = [
   "Leadership",
   "Teamwork",
-  "Communication"]
+  "Communication",
+  "Problem-Solving",
+  "Adaptability",
+  "Time Management",
+  "Attention to Detail",
+  "Critical Thinking",
+  "Creativity",
+  "Collaboration",
+  "Empathy",
+  "Self-Motivation",
+  "Accountability",
+  "Open to Feedback",
+  "Stress Management",
+  "Decision-Making",
+  "Learning Agility",
+];
 const Skils = () => {
   const [showTechSkill, setShowTechSkill] = useState(false);
   const [showSoftSkill, setShowSoftSkill] = useState(false);
 
   const handleTeckToggle = () => {
     setShowTechSkill(!showTechSkill);
-   
   };
   const handleSoftToggle = () => {
     setShowSoftSkill(!showSoftSkill);
-   
   };
   return (
     <section id="skills" className="section ">
       <h2 className="text-4xl font-bold mb-8">Skills</h2>
       <div
-        className="flex flex-row justify-between items-center w-full bg-slate-50/40 p-2"
+        className="flex flex-row justify-between items-center w-full cursor-pointer bg-slate-100/40 p-2"
         onClick={() => handleTeckToggle()}
       >
         <span>Technical Skills</span>{" "}
@@ -52,47 +69,42 @@ const Skils = () => {
         </button>
       </div>
 
-     {
-      <ul className="skillbox grid grid-cols-2 md:grid-cols-5 w-full text-center bg-slate-50/30  ">
-        {
-          showTechSkill &&
-          techSkill.map((skill, index) => (
-            <li key={index} className="py-2 text-lg font-medium">
-              {skill}
-            </li>
-          ))
-        }
-      </ul>
-     }
-     
-     {/* SOFT */}
-     <div
-        className="flex flex-row justify-between items-center w-full mt-4 bg-slate-50/40 p-2"
+      {
+        <ul className="skillbox grid grid-cols-2 md:grid-cols-5 w-full text-center bg-slate-50/30  ">
+          {showTechSkill &&
+            techSkill.map((skill, index) => (
+              <li key={index} className="py-2 text-lg font-medium">
+                {skill}
+              </li>
+            ))}
+        </ul>
+      }
+
+      {/* SOFT */}
+      <div
+        className="flex flex-row justify-between items-center w-full mt-4 cursor-pointer bg-slate-100/40 p-2"
         onClick={() => handleSoftToggle()}
       >
         <span>Soft Skills</span>{" "}
         <button>
           <IoIosArrowDown
             className={
-              showTechSkill ? "duration-300" : "rotate-180 duration-300"
+              showSoftSkill ? "duration-300" : "rotate-180 duration-300"
             }
           />
         </button>
       </div>
 
-     {
-      <ul className="skillbox grid grid-cols-2 md:grid-cols-5 w-full text-center bg-slate-50/30  ">
-        {
-          showSoftSkill &&
-          softSkill.map((skill, index) => (
-            <li key={index} className="py-2 text-lg font-medium">
-              {skill}
-            </li>
-          ))
-        }
-      </ul>
-     }
-     
+      {
+        <ul className="skillbox grid grid-cols-2 md:grid-cols-5 w-full text-center bg-slate-50/30  ">
+          {showSoftSkill &&
+            softSkill.map((skill, index) => (
+              <li key={index} className="py-2 text-lg font-medium">
+                {skill}
+              </li>
+            ))}
+        </ul>
+      }
     </section>
   );
 };
